cbb6416427defa888e3396f9fd7ebb8396eef730
Niv <nivb52@gmail.com>
2022-05-23 17:44:53 +0300

    mock data

--- data.js
+++ data.js
@@ -3,9 +3,8 @@ let d = new Date();
 for (let num = 30; num >= 0; num--){
     d.setHours(-24 * num);
     data.push({
-      date: d.toISOString()
-        .substring(0, 10),
-      value: 1 + Math.random() * 1,
+      date: d.toISOString().substring(0, 10),
+      value: 1 + (Math.random() > 0.7 ? Math.random() - 0.5 : Math.random()),
     });
 }
 export default data;
